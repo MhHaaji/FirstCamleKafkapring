0a33e39e5be69bd2f01e849dab32273a68de4a9d
Haaji <soldier.mh.2001@gmail.com>
2021-09-12 16:04:13 +0430

    :''/

---
+++ src/test/java/com/example/firstcamlekafkapring/FirstCamleKafkapringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstcamlekafkapring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCamleKafkapringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
